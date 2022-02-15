5656ebbf6e9315b4f64d530aa25249e12ee9d946
Cees Voesenek <cees.voesenek@vortech.nl>
2022-02-15 11:27:24 +0100

    Remove trailing whitespace

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 conda env create --name decona -f decona/bin/decona.yml
 
-if [ -d ~/miniconda3/envs/decona ] ; 
+if [ -d ~/miniconda3/envs/decona ] ;
     then
         cp decona/bin/decona ~/miniconda3/envs/decona/bin ;
-elif [ -d ~/miniconda2/envs/decona ] ; 
+elif [ -d ~/miniconda2/envs/decona ] ;
      then
         cp decona/bin/decona ~/miniconda2/envs/decona/bin ;
 else echo "It seems like you have a different version of Conda, please let Saskia know so it can be added!" ;
